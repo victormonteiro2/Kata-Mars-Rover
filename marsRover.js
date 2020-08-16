e964b80c9d4cb9ee9175a680e3726d5db254bce4
Victor Monteiro <victor_monteiro@live.com>
2020-08-16 09:14:41 -0300

    projeto marsRover

---
+++ marsRover.js
@@ -0,0 +1,326 @@
+// Rover object goes here:
+
+let rover = {
+  direction: 'N',
+  position: {
+    x: 0,
+    y: 0
+  },
+  travelLog: [],
+  name: 'Curiosity Rover'
+};
+
+const obstacle1 = {
+  positionObst: {
+    x: 3,
+    y: 0
+  }
+};
+
+const obstacle2 = {
+  position: {
+    x: 2,
+    y: 3
+  }
+};
+
+function commands(rover, orders) {
+  for (let i = 0; i < orders.length; i++) {
+    let order = orders[i];
+    switch (order) {
+      case 'l':
+        turnLeft(rover, order);
+        break;
+      case 'r':
+        turnRight(rover, order);
+        break;
+      case 'f':
+        moveForward(rover, order);
+        break;
+      case 'b':
+        moveBackwards(rover, order);
+        break;
+      default:
+        console.log('Error, type a valid command');
+    }
+  }
+}
+
+function moveForward(rover) {
+  if (rover.direction == 'N') {
+    // DIREÇÃO NORTE - FORWARD**********************************************************
+    console.log('Move Forward was called!');
+    if (rover.position.y > 0 && rover.position.y <= 9) {
+      if (
+        rover.position.y - 1 == obstacle1.positionObst.y &&
+        rover.position.x == obstacle1.positionObst.x
+      ) {
+        console.log("It's a trap! You can't go forward.");
+      } else if (
+        rover.position.y == obstacle2.position.y &&
+        rover.position.x == obstacle2.position.x
+      ) {
+        console.log("It's a trap! You can't go forward.");
+      } else {
+        rover.position.y--;
+        rover.travelLog.push(
+          rover.position.x,
+          rover.position.y,
+          rover.direction
+        );
+      }
+      console.log(`${rover.name} keeps facing ${rover.direction}`);
+    } else {
+      console.log('Fora do tabuleiro');
+      console.log(`${rover.name} is at the same place`);
+    }
+  } else if (rover.direction == 'S') {
+    // DIREÇÃO SUL - FORWARD*************************************************************
+    console.log('Move Forward was called!');
+    if (rover.position.y >= 0 && rover.position.y < 9) {
+      if (
+        rover.position.y + 1 == obstacle1.positionObst.y &&
+        rover.position.x == obstacle1.positionObst.x
+      ) {
+        console.log("It's a trap! You can't go forward.");
+      } else if (
+        rover.position.y + 1 == obstacle2.position.y &&
+        rover.position.x == obstacle2.position.x
+      ) {
+        console.log("It's a trap! You can't go forward.");
+      } else {
+        rover.position.y++;
+        rover.travelLog.push([
+          rover.position.x,
+          rover.position.y,
+          rover.direction
+        ]);
+      }
+      console.log(`${rover.name} keeps facing ${rover.direction}`);
+    } else {
+      console.log('Fora do tabuleiro');
+      console.log(`${rover.name} is at the same place`);
+    }
+  } else if (rover.direction == 'E') {
+    // DIREÇÃO LESTE - FORWARD*************************************************************
+    console.log('Move Forward was called!');
+    if (rover.position.x >= 0 && rover.position.x < 9) {
+      if (
+        rover.position.x + 1 == obstacle1.positionObst.x &&
+        rover.position.y == obstacle1.positionObst.y
+      ) {
+        console.log("It's a trap! You can't go forward.");
+      } else if (
+        rover.position.y == obstacle2.position.y &&
+        rover.position.x == obstacle2.position.x
+      ) {
+        console.log("It's a trap! You can't go forward.");
+      } else {
+        rover.position.x++;
+        rover.travelLog.push([
+          rover.position.x,
+          rover.position.y,
+          rover.direction
+        ]);
+      }
+      console.log(`${rover.name} keeps facing ${rover.direction}`);
+    } else {
+      console.log('Fora do tabuleiro');
+      console.log(`${rover.name} is at the same place`);
+    }
+  } else if (rover.direction == 'W') {
+    // DIREÇÃO OESTE - FORWARD*************************************************************
+    console.log('Move Forward was called!');
+    if (rover.position.x > 0 && rover.position.x <= 9) {
+      if (
+        rover.position.x - 1 == obstacle1.positionObst.x &&
+        rover.position.y == obstacle1.positionObst.y
+      ) {
+        console.log("It's a trap! You can't go forward.");
+      } else if (
+        rover.position.y == obstacle2.position.y &&
+        rover.position.x == obstacle2.position.x
+      ) {
+        console.log("It's a trap! You can't go forward.");
+      } else {
+        rover.position.x--;
+        rover.travelLog.push([
+          rover.position.x,
+          rover.position.y,
+          rover.direction
+        ]);
+      }
+      console.log(`${rover.name} keeps facing ${rover.direction}`);
+    } else {
+      console.log('Fora do tabuleiro');
+      console.log(`${rover.name} is at the same place`);
+    }
+  }
+}
+
+function moveBackwards(rover) {
+  if (rover.direction == 'N') {
+    // DIREÇÃO NORTE - BACKWARDS*************************************************************
+    console.log('Move Backward was called!');
+    if (rover.position.y >= 0 && rover.position.y < 9) {
+      if (
+        rover.position.y + 1 == obstacle1.positionObst.y &&
+        rover.position.x == obstacle1.positionObst.x
+      ) {
+        console.log("It's a trap! You can't go forward.");
+      } else {
+        rover.position.y++;
+        rover.travelLog.push([
+          rover.position.x,
+          rover.position.y,
+          rover.direction
+        ]);
+      }
+      console.log(`${rover.name} keeps facing ${rover.direction}`);
+    } else {
+      console.log('Fora do tabuleiro');
+      console.log(`${rover.name} is at the same place`);
+    }
+  } else if (rover.direction == 'S') {
+    // DIREÇÃO SUL - BACKWARDS*************************************************************
+    console.log('Move Backwards was called!');
+    if (rover.position.y > 0 && rover.position.y <= 9) {
+      if (
+        rover.position.y - 1 == obstacle1.positionObst.y &&
+        rover.position.x == obstacle1.positionObst.x
+      ) {
+        console.log("It's a trap! You can't go backwards.");
+      } else {
+        rover.position.y--;
+        rover.travelLog.push([
+          rover.position.x,
+          rover.position.y,
+          rover.direction
+        ]);
+      }
+      console.log(`${rover.name} keeps facing ${rover.direction}`);
+    } else {
+      console.log('Fora do tabuleiro');
+      console.log(`${rover.name} is at the same place`);
+    }
+  } else if (rover.direction == 'E') {
+    // DIREÇÃO LESTE - BACKWARDS*************************************************************
+    console.log('Move Backwards was called!');
+    if (rover.position.x > 0 && rover.position.x <= 9) {
+      if (
+        rover.position.x - 1 == obstacle1.positionObst.x &&
+        rover.position.y == obstacle1.positionObst.y
+      ) {
+        console.log("It's a trap! You can't go backwards.");
+      } else {
+        rover.position.x--;
+        rover.travelLog.push([
+          rover.position.x,
+          rover.position.y,
+          rover.direction
+        ]);
+      }
+      console.log(`${rover.name} keeps facing ${rover.direction}`);
+    } else {
+      console.log('Fora do tabuleiro');
+      console.log(`${rover.name} is at the same place`);
+    }
+  } else if (rover.direction == 'W') {
+    // DIREÇÃO OESTE - BACKWARDS*************************************************************
+    console.log('Move Backwards was called!');
+    if (rover.position.x >= 0 && rover.position.x < 9) {
+      if (
+        rover.position.x + 1 == obstacle1.positionObst.x &&
+        rover.position.y == obstacle1.positionObst.y
+      ) {
+        console.log("It's a trap! You can't go backwards.");
+      } else {
+        rover.position.x++;
+        rover.travelLog.push([
+          rover.position.x,
+          rover.position.y,
+          rover.direction
+        ]);
+      }
+      console.log(`${rover.name} keeps facing ${rover.direction}`);
+    } else {
+      console.log('Fora do tabuleiro');
+      console.log(`${rover.name} is at the same place`);
+    }
+  }
+}
+
+function turnRight(rover) {
+  console.log('turnRight was called!');
+  switch (rover.direction) {
+    case 'N':
+      rover.direction = 'E';
+      console.log(`${rover.name} is now facing to ${rover.direction}`);
+      break;
+    case 'S':
+      rover.direction = 'W';
+      console.log(`${rover.name} is now facing to ${rover.direction}`);
+      break;
+    case 'E':
+      rover.direction = 'S';
+      console.log(`${rover.name} is now facing to ${rover.direction}`);
+      break;
+    case 'W':
+      rover.direction = 'N';
+      console.log(`${rover.name} is now facing to ${rover.direction}`);
+      break;
+  }
+}
+
+function turnLeft(rover) {
+  console.log('turnLeft was called!');
+  switch (rover.direction) {
+    case 'N':
+      rover.direction = 'W';
+      console.log(`${rover.name} is now facing to ${rover.direction}`);
+      break;
+    case 'S':
+      rover.direction = 'E';
+      console.log(`${rover.name} is now facing to ${rover.direction}`);
+      break;
+    case 'E':
+      rover.direction = 'N';
+      console.log(`${rover.name} is now facing to ${rover.direction}`);
+      break;
+    case 'W':
+      rover.direction = 'S';
+      console.log(`${rover.name} is now facing to ${rover.direction}`);
+      break;
+  }
+}
+
+commands(rover, 'rfffrffflff');
+
+console.log(`O log do ${rover.name} é ${rover.travelLog}`); // continua dando erro de "O log do Curiosity Rover é [object Object]" ** VERIFICAR
+/*
+let teste = {
+  somar: function somar(y, x) {
+    return y + x;
+  },
+  subtrair: function sub(y, x) {
+    return y - x;
+  },
+  multiplicar: function mult(y, x) {
+    return y * x;
+  },
+  addtravellog: function add(y, x, d) {
+    const newtravel = {
+      position: {
+        x: x,
+        y: y
+      },
+      direction: d
+    };
+    return rover.travellog.push(newtravel);
+  }
+};
+
+const vini = teste.somar(2, 4);
+
+console.log(vini);
+*/
